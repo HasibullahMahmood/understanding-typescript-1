b75c8bd76e5e9d19227c64a072226bb771fe6f20
Hasibullah Mahmood 2 <hasibullahmahmod@gmail.com>
2022-01-02 12:07:13 +0300

    2.6 type assignment & type inference

--- app.ts
+++ app.ts
@@ -1,13 +1,14 @@
 function add(num1: number, num2: number, showResult: boolean, phrase: string) {
 	const result = num1 + num2;
 	if (showResult) {
-		console.log(phrase + result);
+		return console.log(phrase + result);
 	} else {
 		return result;
 	}
 }
 
-const number1 = '5';
+let number1: number;
+number1 = 5;
 const number2 = 2.7;
 
-add(+number1, number2, true, 'The result is: ');
+add(number1, number2, true, 'The result is: ');
