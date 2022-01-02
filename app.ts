0af3c81fb42b1efdd7607a6cee0c32c3b55b3666
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2022-01-02 14:52:48 +0300

    2.10 working with tuples

--- app.ts
+++ app.ts
@@ -2,10 +2,12 @@ const person: {
 	name: string;
 	age: number;
 	hobbies: string[];
+	role: [number, string]; // Tuple
 } = {
 	name: 'Hasibullah',
 	age: 25,
 	hobbies: ['Sports', 'Cooking'],
+	role: [0, 'Admin'],
 };
 
 console.log(person.name);
