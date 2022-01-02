9c7861f6a080fd91a4021fd06216a73e57115813
Hasibullah Mahmood 2 <hasibullahmahmod@gmail.com>
2022-01-02 10:39:38 +0300

    lesson 2.2 using types

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('Time to get started...');
+function add(num1: number, num2: number) {
+	return num1 + num2;
+}
+
+const number1 = '5';
+const number2 = 2.7;
+
+const result = add(+number1, number2);
+
+console.log(result);
