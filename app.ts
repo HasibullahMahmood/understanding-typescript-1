fad402fc283a61452f38581c0f15d6205faba814
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2022-01-02 23:15:56 +0300

    2.18 functions as type

--- app.ts
+++ app.ts
@@ -6,3 +6,17 @@ const hasibullah: Person = {
 };
 
 console.log(hasibullah);
+
+function add(n1: number, n2: number) {
+	return n1 + n2;
+}
+
+function printValue(input: any) {
+	console.log(input);
+}
+
+let combinedValues: (a: number, b: number) => number;
+combinedValues = add;
+// combinedValues = printValue;
+
+console.log(add(2, 3));
