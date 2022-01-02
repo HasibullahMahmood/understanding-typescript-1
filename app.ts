754b1aeb6105a81c5983c3c88c6d1db5b99c8eb0
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2022-01-02 22:19:20 +0300

    2.14 literal types

--- app.ts
+++ app.ts
@@ -1,7 +1,11 @@
-const combine = (input1: number | string, input2: number | string) => {
+const combine = (input1: number | string, input2: number | string, operationType: 'addition' | 'multiplication') => {
 	let result;
 	if (typeof input1 === 'number' && typeof input2 === 'number') {
-		result = input1 + input2;
+		if (operationType === 'addition') {
+			result = input1 + input2;
+		} else {
+			result = input1 * input2;
+		}
 	} else {
 		result = input1.toString() + input2.toString();
 	}
@@ -9,5 +13,5 @@ const combine = (input1: number | string, input2: number | string) => {
 	return result;
 };
 
-console.log(combine(30, 26));
-console.log(combine('Max', 'Anna'));
+console.log(combine(30, 26, 'multiplication'));
+console.log(combine('Max', 'Anna', 'addition'));
