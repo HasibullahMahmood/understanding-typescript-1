36a2723c00bc843821d1a3a8bad843d6ba8681a4
Hasibullah Mahmood 2 <hasibullahmahmod@gmail.com>
2022-01-02 11:58:05 +0300

    2.5 working with numbers, boolean and string

--- app.ts
+++ app.ts
@@ -1,13 +1,13 @@
-function add(num1: number, num2: number) {
-	// if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-	// 	throw new Error('Incorrect input!');
-	// }
-	return num1 + num2;
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
+	const result = num1 + num2;
+	if (showResult) {
+		console.log(phrase + result);
+	} else {
+		return result;
+	}
 }
 
 const number1 = '5';
 const number2 = 2.7;
 
-const result = add(+number1, number2);
-
-console.log(result);
+add(+number1, number2, true, 'The result is: ');
