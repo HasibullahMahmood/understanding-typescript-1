328a5cadbefedefe1bffe71823fc33c1da1901d3
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2022-01-02 14:20:11 +0300

    2.9 arrays types

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
 const person: {
 	name: string;
 	age: number;
+	hobbies: string[];
 } = {
 	name: 'Hasibullah',
 	age: 25,
+	hobbies: ['Sports', 'Cooking'],
 };
 
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+	console.log(hobby.toUpperCase());
+}
